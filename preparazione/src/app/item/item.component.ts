f3c85ea438b03b5d5571805f3fc1419236d45223
CrovaxVI <scarpante.niccolo@iisgalvanimi.edu.it>
2020-12-21 11:31:58 +0000

    sesto punto

--- preparazione/src/app/item/item.component.ts
+++ preparazione/src/app/item/item.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../food.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import {Location}from '@angular/common';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent  {
 
-  constructor() { }
+obs:Observable<Object>
+prodotto: any;
+foodServiceObs: any;
+  constructor(public food : FoodService,
+    private route: ActivatedRoute,
+    private service: FoodService,
+    private location : Location) { }
 
   ngOnInit(): void {
+    this.obs = this.route.paramMap;
+    this.obs.subscribe(this.getRouterParam);
+  }
+
+  getRouterParam = (params : ParamMap) =>
+  {
+    let foodId = params.get('id');
+    console.log(foodId);
+
+    this.foodServiceObs = this.service.getFoodId(foodId);
+    this.foodServiceObs.subscribe((data)=>this.prodotto = data);
   }
 
 }
